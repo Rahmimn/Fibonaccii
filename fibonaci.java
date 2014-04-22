993e99a24c6081ec9014ea2c11eea3913ce26063
Rahmimn <rahmimn@gmail.com>
2014-04-22 17:27:55 +0700

    modified sum of fibonacci
    ganti jumlah

--- fibonaci.java
+++ fibonaci.java
@@ -18,19 +18,22 @@ public class Fibonacci {
         // TODO code application logic here
         int x=7;
         int tot=0;
+        int tot2=0;
         int a,b;
         //System.out.println("jumlah deret : ");
         //x=s.nextInt();
         a=1;
         b=1;
-        System.out.println(a);
-        System.out.println(b);
+        tot=2;
+        //System.out.println(a);
+        //System.out.println(b);
         for(int i=1;i<x-1;i++){
             tot=a+b;
-            System.out.println(tot);
+            tot2=tot+tot2;
             a=b;
             b=tot;   
         }
+        System.out.println(tot2+2);
         
     }
     
